53b369438246356f91c8bff23a208450ab367c8a
Lena <93406015+lenosta25@users.noreply.github.com>
2022-04-16 14:35:54 +0300

    first commit info

---
+++ main.js
@@ -0,0 +1,21 @@
+Fancybox.bind("[data-fancybox]", {
+   // Your options go here
+});
+
+const gallery = [
+   {
+      src: "https://lipsum.app/id/2/800x600",
+      thumb: "https://lipsum.app/id/2/80x80",
+      caption: "First image",
+   },
+   {
+      src: "https://lipsum.app/id/3/800x600",
+      thumb: "https://lipsum.app/id/3/80x80",
+      caption: "Second image",
+   },
+   {
+      src: "https://lipsum.app/id/4/800x600",
+      thumb: "https://lipsum.app/id/4/80x80",
+      caption: "Third image",
+   },
+];
