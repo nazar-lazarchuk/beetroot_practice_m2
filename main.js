406af937bb2ae97a0f11248cada09ccddb7b3104
VladyslavVorozhkin <vvorozhkin23@gmail.com>
2022-04-19 18:37:08 +0200

    changes after rewiev

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
     slidesPerView: 3,
-        // // Optional parameters
-        // direction: 'vertical',
-        // loop: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
-      });
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
